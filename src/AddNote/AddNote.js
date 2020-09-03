aadfaaec7b922b7f116ee08bb3764e95ce8c90c8
Russ <18russjames@gmail.com>
2020-09-03 14:19:45 -0700

    started validation

--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -1,12 +1,59 @@
 import React from 'react'
 import NotefulForm from '../NotefulForm/NotefulForm'
 import ApiContext from '../ApiContext'
+import config from '../config'
+import ValidationError from '../ValidationError'
 
 export default class AddNote extends React.Component {
+  static defaultProps = {
+    history: {
+      push: () => { }
+    },
+  }
   
   static contextType = ApiContext
 
+  handleSubmit = event => {
+    event.preventDefault()
+    const newNote = {
+      name: event.target.noteName.value,
+      content: event.target.noteContent.value,
+      folderId: event.target.noteFolderId.value,
+      modified: new Date(),
+    }
+
+    fetch(`${config.API_ENDPOINT}/notes`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(newNote),
+    })
+    .then(response => {
+      if (!response.ok)
+        return response.json().then(event => Promise.reject(event))
+      return response.json()
+    })
+    .then(note => {
+      this.context.addNote(note)
+      this.props.history.push(`/folder/${note.folderId}`)
+    })
+    .catch(error => {
+      console.error({error})
+    })
+  }
+
+  validateNote() {
+    const name = this.context.name.value.trim();
+    if (name.length === 0) {
+      return "name is required";
+    } else if (name.length < 3) {
+      return "name must be at least 3 characters long"
+    }
+  }
+
   render() {
+    const noteNameError = this.validateNote();
     const { folders = [] } = this.context;
     return (
       <section className="addNote">
@@ -18,7 +65,7 @@ export default class AddNote extends React.Component {
             <label htmlFor='note-name-input'>
               note
             </label>
-            <select id='note-select'>
+            <select id='note-select' name="noteFolderId">
               <option value={null}>Folders</option>
               {folders.map(folder =>
                 <option key={folder.id} value={folder.id}>{folder.name}
