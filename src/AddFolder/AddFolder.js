9a17a3c53553e4b1eb63cfb3c9313dbad799aa27
Sean Mathis <malkaviansoapbox@yahoo.com>
2020-09-03 11:04:48 -0500

    installed proptypes, set up addNote/addFolder templates, added functions to app.js

---
+++ src/AddFolder/AddFolder.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function AddFolder(){
+  
+}
+
+export default AddFolder;
---
+++ src/AddNote/AddNote.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function AddNote(){
+
+}
+
+export default AddNote;
